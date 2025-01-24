6d3e73afed28c25221b5c61088d8d8ffb049099b
Bhuvanesh D <146946648+bhuvaneshd32@users.noreply.github.com>
2025-01-24 11:59:43 +0530

    Update settings.py

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -17,6 +17,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Application definition
 
 INSTALLED_APPS = [
